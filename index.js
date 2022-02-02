93a74942b1fbe4251291f573f81b3c49c748d36a
Alex Widener <awidener3@gmail.com>
2022-02-01 20:23:08 -0500

    added needed npm packages, created start of file structure, and began work on index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+const inquirer = require('inquirer');
+const mysql = require('mysql2');
+
+const db = mysql.createConnection({
+    host: 'localhost',
+    user:'root',
+    password:'rootroot',
+    database: 'employees_db'
+});
